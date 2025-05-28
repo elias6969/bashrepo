6380b21b49e089c3c69466a257478058310d63bd
elias6969 <eliasanderssons53@gmail.com>
2025-05-28 15:59:38 +0200

    helloooo

--- gitquick.sh
+++ gitquick.sh
@@ -6,6 +6,6 @@ read -p "Commit message: " commitmsg
 git commit -m "$commitmsg"
 
 echo "Available branches:"
-git branches
+git branch 
 read -p "Push to which branch? " branches
 git push origin "$branches"
