d9384257fbac7d4f64211a304f352d0866c0acf7
elias6969 <eliasanderssons53@gmail.com>
2025-05-28 16:42:49 +0200

    fesfse

--- gitquick.sh
+++ gitquick.sh
@@ -5,7 +5,7 @@ git add .
 read -p "Commit message: " commitmsg
 git commit -m "$commitmsg"
 
-echo "Available branches:"
-git branch 
+echo "Branches:"
+git branch | sed 's/^/* /'
 read -p "Push to which branch? " branches
 git push origin "$branches"
